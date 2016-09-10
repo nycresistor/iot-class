28e4d0e83cb69c61ae6e5ca63ea1fb663efd2392
Bonnie Eisenman <bonnie.eisenman@gmail.com>
2016-09-09 20:24:24 -0400

    Move httpGet into its own func

--- TimeApi/TimeApi.ino
+++ TimeApi/TimeApi.ino
@@ -1,13 +1,34 @@
-/*
- *  Simple HTTP get webclient test
- */
 
 #include <ESP8266WiFi.h>
 
 const char* ssid     = "NYCR24";
 const char* password = "clubmate";
 
-const char* host = "www.timeapi.org";
+char* host = "www.timeapi.org";
+
+void httpGet(String url, char* host) {
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  // This will send the request to the server
+  client.println(String("GET ") + url + " HTTP/1.1");
+  client.print("Host: ");
+  client.println("www.timeapi.org");
+  client.println("User-Agent: curl/7.43.0");
+  client.println();
+  delay(800);
+  
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+}
 
 void setup() {
   Serial.begin(115200);
@@ -33,37 +54,9 @@ void setup() {
   Serial.println(WiFi.localIP());
 }
 
-int value = 0;
 
 void loop() {
   delay(5000);
-  ++value;
 
-  Serial.print("connecting to ");
-  Serial.println(host);
-  
-  // Use WiFiClient class to create TCP connections
-  WiFiClient client;
-  const int httpPort = 80;
-  if (!client.connect(host, httpPort)) {
-    Serial.println("connection failed");
-    return;
-  }
-  
-  // This will send the request to the server
-  client.println(String("GET ") + "/est/now" + " HTTP/1.1");
-  client.print("Host: ");
-  client.println("www.timeapi.org");
-  client.println("User-Agent: curl/7.43.0");
-  client.println();
-  delay(800);
-  
-  // Read all the lines of the reply from server and print them to Serial
-  while(client.available()){
-    String line = client.readStringUntil('\r');
-    Serial.print(line);
-  }
-  
-  Serial.println();
-  Serial.println("closing connection");
+  httpGet("/est/now", host);
 }
